6891e2fa5e69ab6690d8df2387230f8698b62bed
Jason Ramapuram <jason.ramapuram@gmail.com>
2017-11-22 13:37:03 +0100

    add compton and xinit

--- gpu_switch.sh
+++ gpu_switch.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# user=$(who | awk {'print "'$0'" '})
+# echo "user is $user"
+# exit 1
+
 if ! [ $(id -u) = 0 ]; then
     echo "The script need to be run as root / sudo." >&2
     exit 1
@@ -11,6 +15,8 @@ if lsmod | grep -q nvidia ; then
     cp ./lightdm.conf.intel /etc/lightdm/lightdm.conf
     cp ./.xinitrc.intel /home/jramapuram/.xinitrc
     chown jramapuram:jramapuram ~/.xinitrc
+    cp ./compton.conf.glx /home/.config/compton.conf
+    chown jramapuram:jramapuram ~/.config/compton.conf
     systemctl stop lightdm.service
     rmmod nvidia_drm nvidia_uvm nvidia_modeset nvidia
     modprobe bbswitch
@@ -21,6 +27,8 @@ else
     cp ./lightdm.conf.nvidia /etc/lightdm/lightdm.conf
     cp ./.xinitrc.nvidia /home/jramapuram/.xinitrc
     chown jramapuram:jramapuram ~/.xinitrc
+    cp ./compton.conf.noglx /home/.config/compton.conf
+    chown jramapuram:jramapuram ~/.config/compton.conf
     modprobe bbswitch
     echo "ON" > /proc/acpi/bbswitch
     modprobe nvidia_drm nvidia_uvm nvidia_modeset nvidia

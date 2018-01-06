fee3886f8963d2eaa4eba9d01ac6e6f24daddad7
Jason Ramapuram <jason.ramapuram@gmail.com>
2018-01-06 13:34:15 +0100

    fixes for bbswitch

--- gpu_switch.sh
+++ gpu_switch.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# user=$(who | awk {'print "'$0'" '})
-# echo "user is $user"
-# exit 1
-
 if ! [ $(id -u) = 0 ]; then
     echo "The script need to be run as root / sudo." >&2
     exit 1
@@ -13,26 +9,28 @@ if lsmod | grep -q nvidia ; then
     # /etc/X11/xorg.conf.d/90-mhwd.conf symlinks to below
     cp ./90-mhwd.conf.intel /etc/X11/mhwd.d/nvidia.conf
     cp ./lightdm.conf.intel /etc/lightdm/lightdm.conf
-    cp ./.xinitrc.intel ~/.xinitrc
+    cp ./.xinitrc.intel /home/jramapuram/.xinitrc
     chown jramapuram:jramapuram ~/.xinitrc
-    cp ./compton.conf.glx ~/.config/compton.conf
-    chown jramapuram:jramapuram ~/.config/compton.conf
     systemctl stop lightdm.service
     rmmod nvidia_drm nvidia_uvm nvidia_modeset nvidia
     modprobe bbswitch
     echo "OFF" >> /proc/acpi/bbswitch
+    cp ./disable_nvidia.conf /etc/modprobe.d
+    cp ./display_setup.sh.nvidia /etc/lightdm/display_setup.sh
+    cp ./mhwd-gpu.conf.intel /etc/modules-load.d/mhwd-gpu.conf
 else
     echo "trying to load nvidia"
     cp ./90-mhwd.conf.nvidia /etc/X11/mhwd.d/nvidia.conf
     cp ./lightdm.conf.nvidia /etc/lightdm/lightdm.conf
-    cp ./.xinitrc.nvidia ~/.xinitrc
+    cp ./.xinitrc.nvidia /home/jramapuram/.xinitrc
+    cp ./display_setup.sh.nvidia /etc/lightdm/display_setup.sh
     chown jramapuram:jramapuram ~/.xinitrc
-    cp ./compton.conf.noglx ~/.config/compton.conf
-    chown jramapuram:jramapuram ~/.config/compton.conf
     modprobe bbswitch
     echo "ON" > /proc/acpi/bbswitch
     modprobe nvidia_drm nvidia_uvm nvidia_modeset nvidia
     systemctl stop lightdm.service
+    rm /etc/modprobe.d/disable_nvidia.conf
+    cp ./mhwd-gpu.conf.nvidia /etc/modules-load.d/mhwd-gpu.conf
 fi
 
 pkill X
